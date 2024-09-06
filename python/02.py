58e45ca56617a17e83b0af9477c12dac7c493413
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2024-09-06 12:39:16 +0100

    Update 02.py

---
+++ python/02.py
@@ -0,0 +1,10 @@
+def Grade_Calculator(marks):
+    boundaries = [[80, 9],[67, 8],[54, 7],[41, 6],[31, 5],[22, 4],[13, 3],[4, 2],[2, 1]]
+    for i in range(len(boundaries)):
+        if marks >= boundaries[i][0]:
+            grade = boundaries[i][1]
+            if i != 0:
+            #unfinished
+            return 
+            pass
+        if marks 
