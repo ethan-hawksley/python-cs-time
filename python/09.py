9c977c230ac4ecd640b026b1614cd47ef1df4605
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2024-10-04 12:32:06 +0100

    Update 09.py

--- python/09.py
+++ python/09.py
@@ -1,6 +1,6 @@
 def amino_acid():
-    file = open("python/data/amino_acid.txt")
-    dna = file.readline().strip()
+    f = open("python/data/amino_acid.txt")
+    dna = f.readline().strip()
     sequence = input("what acid to check\n")
     if len(dna) % 3 == 0:
         count = 0
@@ -10,5 +10,43 @@ def amino_acid():
         print(count)
 
 
+def shopping_list():
+    import os
+
+    list_path = "python/data/" + input("name of list: ") + ".txt"
+    if not (os.path.exists(list_path)):
+        file_list = open(list_path, "x")  # create file
+        file_list.close()
+    file_list = open(list_path, "r")  # open file to read existing list
+
+    items = [""]
+    _ = items.pop(0)  # prevent ide complaints so it knows the type
+    for line in file_list:
+        items.append(line.strip())  # read file
+    file_list.close()
+    file_list = open(list_path, "w")
+
+    looping = True
+    while looping:
+        match input("v to view, a to add, r to remove, s to stop: "):
+            case "v":
+                print(items)
+            case "a":
+                items.append(
+                    input("what to add: ")
+                )  # \n so newlines are inserted when calling writelines
+            case "r":
+                items.remove(input("what to remove: "))
+            case "s":
+                looping = False  # stop
+            case _:
+                print("not option")
+    for i in items:
+        _ = file_list.write(i)
+        _ = file_list.write("\n")
+    file_list.close()
+
+
 if __name__ == "__main__":
     amino_acid()
+    shopping_list()
